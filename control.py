46ea1b89ed94af7b28496450b9d6e781e00212d6
souleymane <souleymanedialloo@yahoo.fr>
2020-10-14 19:40:44 +0200

    add CRUD CAT

--- control.py
+++ control.py
@@ -16,22 +16,55 @@ class Mainproject:
         data_tags = data_json.get('tags')
         data_cat = [data.get('name') for data in data_tags]
 
-    def save_category(self):
-        r_cat = requests.get('https://fr.openfoodfacts.org/categories&json=1')
-        data_json = r_cat.json()
-        data_tags = data_json.get('tags')
-        data_cat = [data.get('name') for data in data_tags]
-        self.db.save_category(data_cat)
+    # def save_category(self):
+    #     r_cat = requests.get('https://fr.openfoodfacts.org/categories&json=1')
+    #     data_json = r_cat.json()
+    #     data_tags = data_json.get('tags')
+    #     data_cat = [data.get('name') for data in data_tags]
+    #     self.db.save_category(data_cat)
 
     def read(self):
-        pass
+        self.db.read_category()
 
     def updated(self):
-        pass
+        self.db.update_category()
 
-    def test(self):
-        self.save_category()
+    def deleted(self):
+        self.db.delete_category()
+
+    def get_product(self):
+        self.keys = [
+            "id",
+            "product_name",
+            "categories",
+            "nutrition_grades",
+            "ingredients_text",
+            "product_url"
+        ]
+
+        load_data = {
+            'action': 'process',
+            'tagtype_0': 'categories',
+            'tag_contains_0': 'contains',
+            'sort_by': 'unique_scans_n',
+            'countries': 'France',
+            'json': 1,
+            'page': 1
+        }
+
+        r_product = requests.get(
+            'https://fr.openfoodfacts.org/cgi/search.pl', params=load_data)
+        data_json = r_product.json()
+        data_tags = data_json.get('products')
+        data_products = [data.get("ingredients_text") for data in data_tags]
+        print(data_products)
 
 
 c = Mainproject()
-c.save_category()
+# c.save_category()
+# c.updated()
+# c.deleted()
+print("impression produit")
+print(c.get_product())
+
+print(c.read())
--- db_off.py
+++ db_off.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 
 class DbOpenFoodFacts:
-    CATEGORY_SIZE = 10
+    CATEGORY_SIZE = 11
 
     def __init__(self):
         self.db = mysql.connector.connect(
@@ -11,18 +11,35 @@ class DbOpenFoodFacts:
             user='root',
             passwd='rootroot',
             auth_plugin='mysql_native_password',
-            database='BasededonneeOFF',
+            database='test_off_v1',
             charset='utf8')
         self.cursor = self.db.cursor()
 
-    def save_category(self, lst_category):
-        for i in range(1, self.CATEGORY_SIZE):
-            add_category = (
-                "INSERT INTO category (name) VALUES (%s)", ("lst_category[i]",))
-            print(add_category)
-            self.cursor.execute(add_category)
-            self.db.commit()
+    # def save_category(self, lst_category):
+    #     for i in range(1, self.CATEGORY_SIZE):
+    #         add_category = (
+    #             "INSERT INTO Category (categoryId, name) VALUES (%s, %s)")
+    #         print(add_category)
+    #         self.cursor.execute(add_category, (13, lst_category[i],))
+    #         self.db.commit()
 
+    def read_category(self):
+        self.cursor.execute("SELECT * FROM Category")
+        result = self.cursor.fetchall()
+        print("-----------------------------------------------")
+        print("| ID | LIST CATEGORIES                        |")
+        print("-----------------------------------------------")
+        for row in result:
+            print(f"| {row[0]}    |   {row[1]}    |")
+            print("-----------------------------------------------")
+        print("-----------------------------------------------")
 
-# ("INSERT INTO Category (name) VALUES(%s)", ("lst_category[i]",))
-# "INSERT INTO Category" "(name)" "VALUES('{}')".format(lst_category[i]))
+    def update_category(self):
+        my_sql = "UPDATE Category SET name = 'Modifier ma category' WHERE categoryId = 12"
+        self.cursor.execute(my_sql)
+        self.db.commit()
+
+    def delete_category(self):
+        my_sql = "DELETE FROM Category WHERE categoryId = 3000"
+        self.cursor.execute(my_sql)
+        self.db.commit()
